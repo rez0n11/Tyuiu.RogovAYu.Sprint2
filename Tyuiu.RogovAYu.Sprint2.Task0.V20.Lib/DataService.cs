e0a9ee53cb97211df5975c28150e75081cd791b2
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-29 15:50:22 +0500

    maybe maybe maybe

--- Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib/DataService.cs
@@ -1,9 +1,9 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib
 {
-    public class DataService : ISprint2Task0V20
+    public class DataService 
     {
-        public bool[] GetCompareOperations(int x, int y)
+        public object[] GetCompareOperations(int x, int y)
         {
             return [x == y, x != y, x < y, x > y, x <= y, x >= y];
         }
