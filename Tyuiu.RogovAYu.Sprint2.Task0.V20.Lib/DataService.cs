dfada2260dfeed7f553c4eece405431169732e4f
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-29 15:09:10 +0500

    Add project files.

---
+++ Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib
+{
+    public class DataService : ISprint2Task0V20
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            return [x == y, x != y, x < y, x > y, x <= y, x >= y];
+        }
+    }
+}
