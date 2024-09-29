0e0aa5e5356cd834cea310cf3c1b66c4a13dc048
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-29 15:22:28 +0500

    restore (2.0.lib)

--- Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib/DataService.cs
@@ -5,9 +5,7 @@ namespace Tyuiu.RogovAYu.Sprint2.Task0.V20.Lib
     {
         public bool[] GetCompareOperations(int x, int y)
         {
-            object [] array = new object[1];
-            array = [x == y, x != y, x<y, x> y, x <= y, x >= y];
-            return [false,true,false,true,false,true];
+            return [x == y, x != y, x < y, x > y, x <= y, x >= y];
         }
     }
 }
