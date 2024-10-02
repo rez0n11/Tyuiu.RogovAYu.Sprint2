1393ef4703ec59f7f940823f0e0d8a7230ac3bae
REZ0N11\rezon <rez0n11@ya.ru>
2024-10-03 00:53:33 +0500

    2.1fix

--- Tyuiu.RogovAYu.Sprint2.Task1.V26.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint2.Task1.V26.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RogovAYu.Sprint2.Task1.V26.Lib
     {
         public bool[] GetLogicOperations(int a, int b, int c, int d)
         {
-            return [a == b | a == a, b == b & c == c, a == a || a == b, a == b && a == a, !(a == a), a == b ^ a == b];
+            return [a == b | a == a, b == b & c == c, a == a || a == b, a == b && a == a, !(a == b), a == b ^ a == b];
         }
     }
 }
