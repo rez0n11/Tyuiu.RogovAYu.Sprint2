b07ef7d47f5fbe6f0de6c5fd028e534d239864be
REZ0N11\rezon <rez0n11@ya.ru>
2024-10-12 18:54:34 +0500

    2.5

---
+++ Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib
+{
+    public class DataService : ISprint2Task5V2
+    {
+        public string FindMonthSeason(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    { return "Зима"; }
+                case 3:
+                case 4:
+                case 5:
+                    { return "Весна"; }
+                case 6:
+                case 7:
+                case 8:
+                    { return "Лето"; }
+                case 9:
+                case 10:
+                case 11:
+                    { return "Осень"; }
+
+                default:
+                    { return "oor"; }
+            
+            }
+        }
+    }
+}
---
+++ Tyuiu.RogovAYu.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib;
+
+namespace Tyuiu.RogovAYu.Sprint2.Task5.V2
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            DataService ds = new DataService();
+            string result;
+
+            Console.Title = "Task:..v| Рогов А.Ю., ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Рогов Александр Юрьевич |  ПКТб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие: По данному месяцу, определите в какую пору года попадает этот  *\n" +
+                              "*            месяц (Зима, Лето, Весна, Осень).                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.Write("* ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+
+            { //code
+                result = ds.FindMonthSeason(input);
+            }
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"* {result}");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
