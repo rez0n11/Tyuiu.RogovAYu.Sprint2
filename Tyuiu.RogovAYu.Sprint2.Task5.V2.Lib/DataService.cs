3cf0cb12f3a50aff86553677380f91e6f61b508d
REZ0N11\rezon <rez0n11@ya.ru>
2024-10-12 18:55:39 +0500

    2,5

--- Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib/DataService.cs
@@ -10,19 +10,19 @@ namespace Tyuiu.RogovAYu.Sprint2.Task5.V2.Lib
                 case 1:
                 case 2:
                 case 12:
-                    { return "Зима"; }
+                    { return "зима"; }
                 case 3:
                 case 4:
                 case 5:
-                    { return "Весна"; }
+                    { return "весна"; }
                 case 6:
                 case 7:
                 case 8:
-                    { return "Лето"; }
+                    { return "лето"; }
                 case 9:
                 case 10:
                 case 11:
-                    { return "Осень"; }
+                    { return "осень"; }
 
                 default:
                     { return "oor"; }
